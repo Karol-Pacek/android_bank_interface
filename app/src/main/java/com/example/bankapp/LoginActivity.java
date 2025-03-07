27a22df07649cdacfe4ffc6bac31498cba9e58ad
wellino57 <bartek4urban@gmail.com>
2025-03-07 19:37:44 +0100

    wielkie dzięki hubert 𝓼𝓴𝓲𝓫𝓲𝓭𝓲

---
+++ app/src/main/java/com/example/bankapp/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.bankapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        btn = findViewById(R.id.login_button);
+        Intent intent = new Intent(this, MainActivity.class);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Przekierowywanie...", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+    }
+}
